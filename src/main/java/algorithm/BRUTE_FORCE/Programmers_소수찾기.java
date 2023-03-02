7b17892473c401780ea3a8372a077fb1fc7da1f0
cgk95 <kjkj5868@gmail.com>
2023-03-02 21:44:03 +0900

    feat(BRUTE_FORCE) : Programmers_소수찾기, Programmers_피로도 풀이

---
+++ src/main/java/algorithm/BRUTE_FORCE/Programmers_소수찾기.java
@@ -0,0 +1,59 @@
+package algorithm.BRUTE_FORCE;
+
+import java.util.ArrayList;
+
+public class Programmers_소수찾기 {
+    ArrayList<Integer> permutations;
+    String[] number;
+    public int solution(String numbers) {
+        number = numbers.split("");
+        permutations = new ArrayList<>();
+        for (int j = 1; j <= number.length; j++) {
+            myDfs(number, 0, number.length, j);
+        }
+        return permutations.stream().filter(this::isPrime).toArray(Integer[]::new).length;
+    }
+
+    private void myDfs(String[] arr, int depth, int n, int k) {
+        if (depth == k) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < k; i++) {
+                sb.append(arr[i]);
+            }
+            int currNum = Integer.parseInt(sb.toString());
+            if (!permutations.contains(currNum)) {
+                permutations.add(currNum);
+            }
+            return;
+        }
+        for (int i = depth; i < n; i++) {
+            swap(arr, depth, i);
+            myDfs(number, depth + 1, n, k);
+            swap(arr, depth, i);
+        }
+
+    }
+
+    private void swap(String[] arr, int index, int i) {
+        String temp = arr[index];
+        arr[index] = arr[i];
+        arr[i] = temp;
+    }
+
+    private boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int j = 2; j < n; j++) {
+            if (n % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Programmers_소수찾기 sol = new Programmers_소수찾기();
+        System.out.println(sol.solution("011"));
+    }
+}
---
+++ src/main/java/algorithm/BRUTE_FORCE/Programmers_피로도.java
@@ -0,0 +1,33 @@
+package algorithm.BRUTE_FORCE;
+
+public class Programmers_피로도 {
+    int answer=0;
+    boolean[] visited;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        for(int j=1;j<=dungeons.length;j++){ // 몇개의 던전을 순회할 것인지
+            myDfs(dungeons,0, dungeons.length,j,k);
+        }
+        return answer;
+    }
+
+    private void myDfs(int[][] dungeons,int depth,int n,int r,int k) {
+        if(depth==r || k<=0){ // 목표한 갯수의 던전을 돌파했거나 피로도를 모두 소모했을 경우
+            answer=Math.max(answer,depth);
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(!visited[i] && k>=dungeons[i][0]){ // 방문하지 않았고 최소요구 피로도보다 피로도가 많은 경우
+                visited[i]=true;
+                myDfs(dungeons,depth+1,n,r,k-dungeons[i][1]);
+                visited[i]=false;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Programmers_피로도 sol=new Programmers_피로도();
+        System.out.println(sol.solution(80,new int[][] {{80, 20}, {50, 40}, {30, 10}}));
+    }
+}
