c0ab44c357a46f373620a4d9dc6b07525e7731fa
cgk95 <kjkj5868@gmail.com>
2023-04-14 13:07:48 +0900

    feat(GREEDY) : Programmers_단속카메라 풀이

--- src/main/java/algorithm/GREEDY/Programmers_단속카메라.java
+++ src/main/java/algorithm/GREEDY/Programmers_단속카메라.java
@@ -4,22 +4,17 @@ import java.util.Arrays;
 
 public class Programmers_단속카메라 {
     public int solution(int[][] routes) {
-        int answer = 1;
+        int answer = 0, flag = -30001;
         for (int[] arr : routes) { // 역주행하는 미친놈 제대로 뒤집기
             Arrays.sort(arr);
         }
         Arrays.sort(routes, (o1, o2) -> o1[0] - o2[0]);
-        int[] curr = routes[0];
-        for (int i = 1; i < routes.length; i++) {
-            if (curr[0] <= routes[i][0] && routes[i][0] <= curr[1]) { // 겹치는 부분 존재 시
-                curr = new int[]{routes[i][0], routes[i - 1][1]}; // 업데이트
-                if (routes[i][1] <= curr[1]) { // 완전한 부분 집합인경우
-                    curr[1] = routes[i][1]; // 땡기기
-                }
-            } else {
+        for (int i = 0; i < routes.length; i++) {
+            if (flag < routes[i][0]) { // 겹치지 않으면
                 answer++;
-                curr = routes[i];
+                flag = routes[i][1];
             }
+            flag = Math.min(flag, routes[i][1]);
         }
         return answer;
     }
