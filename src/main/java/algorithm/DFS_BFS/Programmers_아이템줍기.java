31667291cad9bd86ced15c3215a8b9699d60c1ad
cgk95 <kjkj5868@gmail.com>
2023-04-14 22:33:30 +0900

    feat(BFS) : Programmers_아이템줍기 풀이, ㄷ자가 붙어있다고 착각할 수 있으므로 2배로 불려서 계산하는 것에 주의하자

---
+++ src/main/java/algorithm/DFS_BFS/Programmers_아이템줍기.java
@@ -0,0 +1,66 @@
+package algorithm.DFS_BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Programmers_아이템줍기 {
+    char[][] graph;
+
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        graph = new char[101][101];
+        for (int[] ints : rectangle) {
+            int x1 = ints[0];
+            int y1 = ints[1];
+            int x2 = ints[2];
+            int y2 = ints[3];
+            draw(x1 * 2, y1 * 2, x2 * 2, y2 * 2);
+        }
+        return myBFS(characterX * 2, characterY * 2, itemX * 2, itemY * 2);
+    }
+
+    private void draw(int x1, int y1, int x2, int y2) {
+        for (int i = x1; i <= x2; i++) {
+            for (int j = y1; j <= y2; j++) {
+                if (graph[j][i] == '1') {
+                    continue;
+                }
+                graph[j][i] = '1';
+                if (i == x1 || i == x2 || j == y1 || j == y2) { // 테두리인 경우
+                    graph[j][i] = '2';
+                }
+            }
+        }
+    }
+
+    private int myBFS(int x, int y, int targetX, int targetY) {
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+        Queue<Integer[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[101][101];
+        q.add(new Integer[]{y, x, 0});
+
+        while (!q.isEmpty()) {
+            Integer[] curr = q.poll();
+            int currY = curr[0];
+            int currX = curr[1];
+            int currCount = curr[2];
+            if (currY == targetY && currX == targetX) { // 도착 시
+                return currCount / 2;
+            }
+
+            for (int k = 0; k < 4; k++) {
+                int nextX = currX + dx[k];
+                int nextY = currY + dy[k];
+                if (nextY < 0 || nextX < 0 || nextY >= graph.length || nextX >= graph[0].length) {
+                    continue;
+                }
+                if (visited[nextY][nextX] || graph[nextY][nextX] != '2') { // 방문 하였거나 테두리가 아닌 경우
+                    continue;
+                }
+                q.offer(new Integer[]{nextY, nextX, ++currCount});
+                visited[nextY][nextX] = true;
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/algorithm/Programmers_아이템줍기Test.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+import algorithm.DFS_BFS.Programmers_아이템줍기;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class Programmers_아이템줍기Test {
+    Programmers_아이템줍기 sol = new Programmers_아이템줍기();
+    @Test
+    void test01(){
+        assertThat(sol.solution(new int[][] {{1, 1, 7, 4}, {3, 2, 5, 5}, {4, 3, 6, 9}, {2, 6, 8, 8}},1,3,7,8))
+                .isEqualTo(17);
+    }
+
+}
