1b5b6c2fd2d634ba5319ae140d073bfb75d94e10
cgk95 <kjkj5868@gmail.com>
2023-05-08 18:29:18 +0900

    feat(DFS) : Programmers_점프와순간이동 풀이

---
+++ src/main/java/algorithm/Programmers_점프와순간이동.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+public class Programmers_점프와순간이동 {
+    public int solution(int n) {
+        return Integer.bitCount(n);
+    }
+
+    public static void main(String[] args) {
+        Programmers_점프와순간이동 sol = new Programmers_점프와순간이동();
+        System.out.println(sol.solution(6));
+    }
+}
