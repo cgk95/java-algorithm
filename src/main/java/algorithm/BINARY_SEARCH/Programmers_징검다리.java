c57f41ac49656aa9ee24bfbf143b6153ef3200fe
cgk95 <kjkj5868@gmail.com>
2023-08-31 16:28:04 +0900

    feat(BIN_SEARCH) :: Programmers_징검다리.java 다시 풀이

--- src/main/java/algorithm/BINARY_SEARCH/Programmers_징검다리.java
+++ src/main/java/algorithm/BINARY_SEARCH/Programmers_징검다리.java
@@ -1,7 +1,7 @@
 package algorithm.BINARY_SEARCH;
 
 import java.util.Arrays;
-
+// TODO :: 다시 풀어보기
 public class Programmers_징검다리 {
     public int solution(int distance, int[] rocks, int n) {
         Arrays.sort(rocks);
@@ -15,19 +15,18 @@ public class Programmers_징검다리 {
             int removedRock = 0;
             int prevRock = 0;
             for (int rock : rocks) {
-                if (rock - prevRock < mid) { // mid 값보다 작은 거리 사이에 바위가 있으면
+                if (rock - prevRock < mid) { // mid 값보다 작은 최소거리를 만드는 바위가 있으면
                     removedRock++;  // 해당 바위를 제거
-                } else { // mid 값보다 큰 거리 사이에 바위가 있으면
-                    prevRock = rock;  // 현재 바위를 이전 바위로 설정
+                } else {
+                    prevRock = rock;  // 현재 바위를 이전 바위로 설정하고 다시 최소거리를 체크
                 }
             }
             // 마지막 바위와 도착지점 사이의 거리를 체크
-            if (distance - prevRock < mid) { // mid 값보다 작은 거리 사이에 바위가 있으면
+            if (distance - prevRock < mid) { // mid 값보다 최소거리를 만드는 바위가 있으면
                 removedRock++; // 해당 바위를 제거
             }
-            // 제거한 바위 수가 n보다 크거나 같으면 mid 값을 answer에 저장하고,
-            // mid 값보다 작은 거리 사이에 바위가 너무 많이 남아있으면 mid 값 감소
-            if (removedRock <= n) {
+
+            if (removedRock == n) {
                 answer = Math.max(answer, mid);
                 start = mid + 1;
             } else {
